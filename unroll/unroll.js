a973f8402d48f783892ef6f05a67fbd755ce7033
Steven <srmcnamara7@gmail.com>
2023-09-12 11:23:40 -0400

    unroll completed

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,30 @@
 function unroll(squareArray) {
+    let end = squareArray.length;
 
+    let start = 0;
+    let newArray = [];
+
+    while (end > start) {
+        for (let j = start; j < end; j++) {
+            newArray.push(squareArray[start][j]);
+        }
+
+        for (let i = start + 1; i < end; i++) {
+            newArray.push(squareArray[i][end - 1]);
+        }
+
+        end--;
+
+        for (let j = end - 1; j >= start; j--) {
+            newArray.push(squareArray[end][j]);
+        }
+        for (let i = end - 1; i > start; i--) {
+            newArray.push(squareArray[i][start]);
+        }
+        start++;
+    }
+
+    return newArray;
 }
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -1,9 +1,52 @@
 const unroll = require("./unroll");
+const square = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+];
+
+const smallerSquare = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"],
+];
+
+const bigAssSquare = [
+    [1, 2, 3, 4, 5, 6],
+    [7, 8, 9, 10, 11, 12],
+    [13, 14, 15, 16, 17, 18],
+    [19, 20, 21, 22, 23, 24],
+    [25, 26, 27, 28, 29, 30],
+    [31, 32, 33, 34, 35, 36],
+];
 
 describe("#unroll", function () {
-
-  it("is a function", function () {
-    expect(typeof unroll).toEqual("function");
-  });
-
+    it("is a function", function () {
+        expect(typeof unroll).toEqual("function");
+    });
+    it("works for square", function () {
+        expect(unroll(square)).toEqual([
+            1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10,
+        ]);
+    });
+    it("works for smallerSquare", function () {
+        expect(unroll(smallerSquare)).toEqual([
+            "a",
+            "b",
+            "c",
+            "f",
+            "i",
+            "h",
+            "g",
+            "d",
+            "e",
+        ]);
+    });
+    it("works for bigAssSquare", function () {
+        expect(unroll(bigAssSquare)).toEqual([
+            1, 2, 3, 4, 5, 6, 12, 18, 24, 30, 36, 35, 34, 33, 32, 31, 25, 19,
+            13, 7, 8, 9, 10, 11, 17, 23, 29, 28, 27, 26, 20, 14, 15, 16, 22, 21,
+        ]);
+    });
 });
